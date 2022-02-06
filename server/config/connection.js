0093a35175d5c00f7610ccb541893db968c4302b
Leia <87383466+klynmc@users.noreply.github.com>
2022-02-06 16:35:45 -0600

    trying this

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // localhost would not load graphql so I replaced it with 127.0.0.1:27017
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true, 
   useUnifiedTopology: true,
   useCreateIndex: true,
