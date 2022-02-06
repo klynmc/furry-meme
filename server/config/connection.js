c577a823be19f7172bdf3fe1a7645910a5b6f88b
Leia <87383466+klynmc@users.noreply.github.com>
2022-02-06 16:23:00 -0600

    changed to localhost

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // localhost would not load graphql so I replaced it with 127.0.0.1
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/furry-meme', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/furry-meme', {
   useNewUrlParser: true, 
   useUnifiedTopology: true,
   useCreateIndex: true,
